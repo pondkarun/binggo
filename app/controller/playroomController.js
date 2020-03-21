fc49c3fd2df96fa002ea9584356e2b54cf27a410
stock <pondkarun@gmail.com>
2020-03-22 01:19:29 +0700

    playRoom

--- app/controller/playroomController.js
+++ app/controller/playroomController.js
@@ -1,14 +1,31 @@
-﻿app.controller('playroomController', function ($scope, $location) {
+﻿app.controller('playroomController', function($scope, $location) {
 
     $scope.tablenum = [];
 
-    $scope.vote = function () {
+    this.init = () => {}
+
+    $scope.vote = function() {
 
     }
-    $scope.random = function () {
-
+    $scope.random = function() {
+        $scope.tablenum = [];
+        var nums = []
+        for (let x = 1; x < 100; x++) {
+            nums.push(x)
+        }
+        var i = nums.length;
+        var j = 0;
+        while (i--) {
+            if ($scope.tablenum.length < 25) {
+                j = Math.floor(Math.random() * (i + 1));
+                $scope.tablenum.push(nums[j]);
+                nums.splice(j, 1);
+            } else {
+                break;
+            }
+        }
     }
-    $scope.select = function () {
+    $scope.select = function() {
 
     }
 
